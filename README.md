244aa3d05c8c737d39e6397cf79988262162cd6c
Paul Aguilar <aguilarpgc@gmail.com>
2018-11-08 14:44:33 -0500

    rewrite

--- README.md
+++ README.md
@@ -1,22 +1,63 @@
 # TimeLapse
-Simple `Int` extension with time lapse description (SPANISH version)
+
+Simple `Int` extension with time lapse description
 
 Converts `Int` (seconds) to a formatted short length `String`
 
-### Usage
+## Usage
 
-10 seg  = `10.timeLapseFormat()`   
+### Default
 
-30 min = `1800.timeLapseFormat()`
+* **Seconds** : `print(10.shortTimeLapse())`   10s
 
-12 hrs = `43200.timeLapseFormat()`
+* **Minutes** : `print(1800.shortTimeLapse())`   30m
 
-2 días = `172_800.timeLapseFormat()` 
+* **Hours**  : `print(43200.shortTimeLapse())`   12h
 
-1 sem = `604_800.timeLapseFormat()`
+* **Days**  : `print(172_800.shortTimeLapse())`    2d
 
-6 meses = `15_552_000.timeLapseFormat()` 
+* **Weeks**  : `print(604_800.shortTimeLapse())`   1w
 
-1 año = `31_536_000.timeLapseFormat()` 
+* **Months**  : `print(15_552_000.shortTimeLapse())`   6mo
+
+* **Years**  : `print(31_536_000.shortTimeLapse())`   1y
+
+### Custom
+
+Conforts to the protocol `TimeLapseFormat` and assign it to `Int.timeLapseFormat`
+
+#### Example
+
+```
+struct CustomTimeLapseFormat: TimeLapseFormat {
+
+    var now: String = "now"
+    var second: String = "sec"
+    var minute: String = "min"
+    var hour: String = "hour"
+    var day: String = "d"
+    var week: String = "w"
+    var month: String = "mon"
+    var year: String = "y"
+}
+```
+Then:
+
+`TimeLapse.format = CustomTimeLapseFormat()`
+
+Result: 
+
+* `0.shortTimeLapse()` now
+* `10.shortTimeLapse()` 10sec
+* `1800.shortTimeLapse()` 30min
+* `7200.shortTimeLapse()` 2hour
+* `604_799.shortTimeLapse()` 6d
+* `2_591_999.shortTimeLapse()` 4w
+* `31_535_999.shortTimeLapse()` 12mon
+* `630_720_000.shortTimeLapse()` 20y
+
+Change back to default format
+
+`TimeLapse.setDefaultTimeLapseFormat()`
 
 
--- Sources/TimeLapse.swift
+++ Sources/TimeLapse.swift
@@ -5,58 +5,70 @@
 //  Created by Paul Aguilar on 9/25/18.
 //
 
+
+public struct TimeLapse {
+    
+    public static var format: TimeLapseFormat = TimeLapseFormatDefault()
+    
+    public static func setDefaultTimeLapseFormat() {
+        
+        TimeLapse.format = TimeLapseFormatDefault()
+    }
+}
+
 public extension Int {
     
-    public func timeLapseFormat() -> String {
+    public func shortTimeLapse() -> String {
         
         let seconds: Int = self
+        let format = TimeLapse.format
         
         if seconds < Int.secondsOnADay {
             
             if seconds < 10 {
                 
-                return "ahora"
+                return format.now
                 
             } else if seconds < 60 {
                 
-                return "\(seconds) seg"
+                return "\(seconds)\(format.second)"
                 
             } else if seconds < Int.secondsOnAnHour {
                 
                 let min: Int = seconds / 60
                 
-                return "\(min) min"
+                return "\(min)\(format.minute)"
                 
             } else {
                 
                 let hours: Int = seconds / Int.secondsOnAnHour
                 
-                return "\(hours) \(hours == 1 ? "hr" : "hrs")"
+                return "\(hours)\(format.hour)"
             }
             
         } else if seconds < Int.secondsOnAWeek {
             
             let days = seconds / (Int.secondsOnADay)
             
-            return "\(days) \(days == 1 ? "día" : "días")"
+            return "\(days)\(format.day)"
             
         } else if seconds < Int.secondsOnAMonth {
             
             let weeks = seconds / (Int.secondsOnAWeek)
             
-            return "\(weeks) sem"
+            return "\(weeks)\(format.week)"
             
         } else if seconds < Int.secondsOnAYear {
             
             let months = seconds / (Int.secondsOnAMonth)
             
-            return "\(months) \(months == 1 ? "mes" : "meses")"
+            return "\(months)\(format.month)"
             
         } else {
             
             let years = seconds / (Int.secondsOnAYear)
             
-            return "\(years) \(years == 1 ? "año" : "años")"
+            return "\(years)\(format.year)"
         }
     }
 }
@@ -69,4 +81,30 @@ private extension Int {
     static let secondsOnAMonth =  2_592_000  // 60 * 60 * 24 * 30
     static let secondsOnAYear  = 31_536_000  // 60 * 60 * 24 * 365
     
+}
+
+
+public protocol TimeLapseFormat {
+    
+    var now    : String { get }
+    var second : String { get }
+    var minute : String { get }
+    var hour   : String { get }
+    var day    : String { get }
+    var week   : String { get }
+    var month  : String { get }
+    var year   : String { get }
+    
+}
+
+public struct TimeLapseFormatDefault: TimeLapseFormat {
+    
+    public let now    = "just now"
+    public let second = "s"
+    public let minute = "m"
+    public let hour   = "h"
+    public let day    = "d"
+    public let week   = "w"
+    public let month  = "mo"
+    public let year   = "y"
 }
